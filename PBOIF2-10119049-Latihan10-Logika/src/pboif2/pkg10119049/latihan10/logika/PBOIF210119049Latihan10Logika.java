e523ab207bdead823548bc736f26da872e07120d
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:44:10 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan10-Logika/src/pboif2/pkg10119049/latihan10/logika/PBOIF210119049Latihan10Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan10.logika;
+
+/**
+ * @author
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program menampilkan hasil operator logika.
+ */
+public class PBOIF210119049Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
